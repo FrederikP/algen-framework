3dd43ab90ea21ebf84e73a9fe7f5925fb7f05451
Lennart Hensler <lennarthensler@aol.com>
2015-09-21 06:12:13 -0700

    Tried to improve DPH with array buckets, but the table has weird behaviour

--- hashtable/lennart_hash_map.h
+++ hashtable/lennart_hash_map.h
@@ -17,10 +17,10 @@ private:
 	size_t size;
 	
 public:
-	void initialize(size_t M, size_t bucketAmount) {
+	void initialize(size_t capacity, size_t bucketAmount) {
 		std::vector<size_t> primes;
-		// Choose first prime >= M
-		primesieve::generate_n_primes<size_t>(1, M, &primes);
+		// Choose first prime >= capacity
+		primesieve::generate_n_primes<size_t>(1, capacity, &primes);
 		prime = primes[0];
 		
 		size = bucketAmount;
@@ -55,6 +55,9 @@ public:
 		std::uniform_int_distribution<size_t> uniform_dist(1, prime - 1);
 		random = uniform_dist(engine);
 	}
+	size_t getPrime() {
+		return prime;
+	}
 	size_t operator()(size_t& x) const {
 		return random * x % prime;
 	}
@@ -63,27 +66,38 @@ public:
 template <typename Key, typename T>
 class element {
 private:
-	Key key;
-	T value;
+	Key _key;
+	T _value;
 	bool initialized;
 	bool deleteFlag;
 public:
-	element(Key& key, T& value) : key(key), value(value), initialized(false), deleteFlag(false) { }
-	element() : key(0), value(0), initialized(false), deleteFlag(false) {}
+	element() {
+		_key = 0;
+		_value = 0;
+		initialized = false;
+		deleteFlag = false;
+	}
+	element(Key& key, T& value) {
+		_key = key;
+		_value = value;
+		initialized = false;
+		deleteFlag = false;
+	}
 	
 	Key& getKey() {
-		return key;
+		return _key;
 	}
 	
 	T& getValue() {
-		return value;
+		return _value;
 	}
 
 	bool isInitialized() {
 		return initialized;
 	}
 
-	void initialize() {
+	void initialize(Key& key) {
+		_key = key;
 		initialized = true;
 	}
 
@@ -102,26 +116,34 @@ class bucket {
 private:
 	element_hash_function elementHashFunction;
 	size_t size;
-	element<Key, T> elements[];
+	size_t _count;
+	element<Key, T>* elements;
 public:
 	bucket() : bucket(0) { }
-	bucket(size_t intendedSize) {
-		size_t minimumSize = 10;
-		size_t max = std::max(minimumSize, intendedSize);
-		size = max * (max - 1) + 1;
-		elementHashFunction.initialize(size);
-		elements[size] = { };
+	bucket(size_t initialElementAmount) {
+		_count = 0;
+		size_t min = 10;
+		size_t max = std::max(min, initialElementAmount);
+		size_t intendedSize = max * (max - 1) + 1;
+		elementHashFunction.initialize(intendedSize);
+		size = elementHashFunction.getPrime();
+
+		elements = new element<Key, T>[size];
+		for(size_t i = 0; i < size; i++) {
+			elements[i] = element<Key, T>();
+		}
 	}
 	
 	T& getValue(size_t preHash, Key key) {
 		size_t elementIndex = elementHashFunction(preHash);
 		element<Key, T> element = elements[elementIndex];
+		if (!element.isInitialized()) {
+			element.initialize(key);
+			_count++;
+		}
 		// If this is not the case something with the dynamic rehashing didn't work out
 		assert(element.getKey() == key);
 		_unused(key);
-		if (!element.isInitialized()) {
-			element.initialize();
-		}
 		return element.getValue();
 	}
 
@@ -147,9 +169,14 @@ public:
 			assert(element.getKey() == key);
 			_unused(key);
 			element.markDeleted();
+			_count--;
 			return 1;
 		}
 		return 0;
+	}
+
+	size_t count() {
+		return _count;
 	}
 };
 
@@ -158,10 +185,9 @@ class DPH_with_array_buckets : public hashtable<Key, T> { // DPH = Dynamic Perfe
 private:
 	PreHashFcn preHashFcn;
 	bucket_hash_function bucketHashFunction;
-	size_t M;
-	size_t count;
+	size_t capacity;
 	size_t bucketAmount;
-	bucket<Key, T> buckets[];
+	bucket<Key, T>* buckets;
 	
 public:
     virtual ~DPH_with_array_buckets() = default;
@@ -171,23 +197,23 @@ public:
         using Factory = common::contender_factory<hashtable<Key, T>>;
         list.register_contender(Factory("DPH_with_array_buckets", "DPH_with_array_buckets",
             [](){ 
-				size_t initialM = 10;
-				size_t s = 100;
-				return new DPH_with_array_buckets(initialM, s); 
+				size_t initialElementAmount = 1000;
+				size_t initialBucketAmount = 100;
+				return new DPH_with_array_buckets(initialElementAmount, initialBucketAmount); 
 			}
         ));
     }
 	
-    DPH_with_array_buckets(size_t initialM, size_t initialBucketAmount) : hashtable<Key, T>() {
-		M = initialM;
-		count = 0;
+    DPH_with_array_buckets(size_t initialElementAmount, size_t initialBucketAmount) : hashtable<Key, T>() {
+		capacity = initialElementAmount;
 		bucketAmount = initialBucketAmount;
 
-		bucketHashFunction.initialize(M, bucketAmount);
-		
-		buckets[bucketAmount] = { };
+		bucketHashFunction.initialize(capacity, bucketAmount);
+
+		size_t initialElementPerBucketAmount = capacity / bucketAmount;
+		buckets = new bucket<Key, T>[bucketAmount];
 		for (size_t i = 0; i < bucketAmount; i++) {
-			buckets[i] = bucket<Key, T>();
+			buckets[i] = bucket<Key, T>(initialElementPerBucketAmount);
 		}
     }
 		
@@ -217,8 +243,12 @@ public:
 		return bucket.erase(preHash, key);
     }
 
-    size_t size() const override { 
-		return count;
+    size_t size() const override {
+		size_t overallCount = 0;
+		for (size_t i = 0; i < bucketAmount; i++) {
+			overallCount += buckets[i].count();
+		}
+		return overallCount;
 	}
 
     void clear() override { 
